2e445362112c4abb3a5423c0fb275b813b40027b
Bhashitha Ranasinghe <Bhashitha@ebuilder.local>
2023-02-25 16:27:34 +0530

    init

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,24 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "aws01": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "function": {
+    "graphQlLambdaAuthorizer7e930586": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "graphQlLambdaAuthorizer993cf135": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
